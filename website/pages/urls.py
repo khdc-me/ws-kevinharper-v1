29dc6e314aebc9bdf142dc12b03f6f7a7b5028f2
Kevin <kevinharperg@gmail.com>
2018-11-04 19:24:30 -0700

    Placed header on my-projects.html pg

--- website/pages/urls.py
+++ website/pages/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('experience/', views.experience, name='experience'),
     path('my-portal/', views.my_portal, name='my-portal'),
+    path('my-projects/', views.my_projects, name='my-projects'),
 ]
